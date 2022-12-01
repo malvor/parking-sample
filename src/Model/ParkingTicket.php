6c8d15dd87e4af4ea19af6519ad2eed3cfb48714
Dannkru <Dannkrupa@gmail.com>
2022-12-01 09:44:18 +0100

    add ParkingTicket class

--- src/Model/ParkingTicket.php
+++ src/Model/ParkingTicket.php
@@ -5,5 +5,58 @@ namespace App\Model;
 
 class ParkingTicket
 {
+    private $parkingStartDate;
+    private $parkingEndDate;
+    private $parking;
+    private $vehicle;
+    private $price;
+    
+    public function __construct (
+        DateTime $parkingStartDate, 
+        ParkingInterface $parking,
+        VehicleInterface $vehicle,
+        DateTime $parkingEndDate = null,
+        Money $price = null
+    ) {
+        $this->parkingStartDate = $parkingStartDate;
+        $this->parkingEndDate = $parkingEndDate;
+        $this->parking = $parking;
+        $this->vehicle = $vehicle;
+        $this->price = $price;
+    }
 
+    public function getParkingStartDate()
+    {
+        return $this->parkingStartDate;
+    }
+
+    public function setParkingEndDate($parkingEndDate)
+    {
+        $this->parkingEndDate = $parkingEndDate;
+    }
+
+    public function getParkingEndDate()
+    {
+        return $this->parkingEndDate;
+    }
+
+    public function getParking()
+    {
+        return $this->parking;
+    }
+
+    public function getVehicle()
+    {
+        return $this->vehicle;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
 }
